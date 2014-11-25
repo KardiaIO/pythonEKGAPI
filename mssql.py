4e57122f9941dbfba7e52e3cb6921097e5f0f99a
radicalsauce <krhale@gmail.com>
2014-11-24 16:51:39 -0800

    Updated query logic

--- mssql.py
+++ mssql.py
@@ -5,4 +5,5 @@ def connect(self, startTime, endTime):
   conn = pypyodbc.connect('Driver=FreeTDS;Server=ekgsql.cloudapp.net;port=1433;uid=ekgwebapp;pwd=ekgsqlserver1MSSS;database=SampleData')
   # send queries along this connection - this sample query grabs the first row of data from the table
   print 'Returning query results...'
-  results = conn.cursor().execute('SELECT x, y FROM SampleData.dbo.sampleekg WHERE x >= {startTime} AND x < {endTime}').fetchall()
+  results = conn.cursor().execute('SELECT x, y FROM SampleData.dbo.sampleekg WHERE x >= ' + startTime + ' AND x < ' + endTime).fetchall()
+  return results
