1156bbb99c0f151ad8bd3928ccf999afc765736b
Cory Asato <czasato@gmail.com>
2014-12-30 19:58:22 -0800

    Postgres linked, Node.js talking

--- mssql.py
+++ mssql.py
@@ -1,9 +1,7 @@
-import pypyodbc
-
-def connect(self, startTime, endTime):
-  # fill this in
-  conn = pypyodbc.connect('Driver=FreeTDS;Server=ekgsql.cloudapp.net;port=1433;uid=ekgwebapp;pwd=ekgsqlserver1MSSS;database=SampleData')
-  # send queries along this connection - this sample query grabs the first row of data from the table
-  print 'Returning query results...'
-  results = conn.cursor().execute('SELECT x, y FROM SampleData.dbo.sampleekg WHERE x >= ' + startTime + ' AND x < ' + endTime).fetchall()
-  return results
+# def connect(self, startTime, endTime):
+#   # fill this in
+#   conn = pypyodbc.connect('Driver=FreeTDS;Server=ekgsql.cloudapp.net;port=1433;uid=ekgwebapp;pwd=ekgsqlserver1MSSS;database=SampleData')
+#   # send queries along this connection - this sample query grabs the first row of data from the table
+#   print 'Returning query results...'
+#   results = conn.cursor().execute('SELECT x, y FROM SampleData.dbo.sampleekg WHERE x >= ' + startTime + ' AND x < ' + endTime).fetchall()
+#   return results
--- server.py
+++ server.py
@@ -2,7 +2,27 @@
 ## our Node.js server using Zerorpc
 
 import zerorpc
-from mssql import connect
+import psycopg2
+# from mssql import connect
+try:
+    conn = psycopg2.connect("dbname='ddkcjaloac929s' user='jtxtmxsnjirunx' host='ec2-54-83-23-169.compute-1.amazonaws.com' password='q8ojXZQTlVDqQSPy3y48CIRK0S' port='5432'")
+except:
+    print "I am unable to connect to the database"
+
+def connect():  
+  cursor = conn.cursor()
+
+  try:
+    cursor.execute(" " "SELECT * from users" " ")
+  except:
+    print "Can't select from users"
+
+  rows = cursor.fetchall()
+  return rows
+
+
+
+
 
 class rpc(object):
     print "Python server ENGAGE!"
@@ -14,7 +34,7 @@ class rpc(object):
 
     def nodeRequest(self, startTime, endTime):
         print "Node data request received, response sending..."
-        return connect(self, startTime, endTime)   
+        return connect()   
 
 server = zerorpc.Server(rpc())
 server.bind("tcp://0.0.0.0:4242")
